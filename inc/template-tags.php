87bd96805d7be7ce5d55a20bf00fcbc8df048b50
Joseph Casabona <josephcasabona@Josephs-MacBook-Pro.local>
2017-09-29 19:29:59 -0400

    Fixed date issue and editor styles

---
+++ inc/template-tags.php
@@ -0,0 +1,27 @@
+<?php 
+
+function understrap_posted_on() {
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+        $time_string = '<time class="updated" datetime="%3$s">%4$s</time>';
+        $time_string = sprintf( $time_string,
+            esc_attr( get_the_modified_date( 'c' ) ),
+            esc_html( get_the_modified_date() )
+        );
+	} else {
+        $time_string = sprintf( $time_string,
+            esc_attr( get_the_date( 'c' ) ),
+            esc_html( get_the_date() )
+        );
+    }
+
+	$posted_on = sprintf(
+		esc_html_x( 'Posted on %s', 'post date', 'understrap' ),
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	);
+	$byline = sprintf(
+		esc_html_x( 'by %s', 'post author', 'understrap' ),
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	);
+	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . 'BUTTS</span>'; // WPCS: XSS OK.
+}
